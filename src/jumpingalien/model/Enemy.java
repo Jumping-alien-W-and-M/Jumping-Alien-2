29d386912d7b7eab03e55a5d7f61aa5e57cd6ee3
Michiel Bollen <michielbollen@student.kuleuven.be>
2015-04-06 11:47:19 +0200

    aanpassing aan shark

--- src/jumpingalien/model/Enemy.java
+++ src/jumpingalien/model/Enemy.java
@@ -78,6 +78,7 @@ public abstract class Enemy extends GameObject {
 	 * 			If dt isn't a valid time interval to advance the time with.
 	 * 			| !isValidDt(dt)
 	 */
+	@Override
 	public void advanceTime(double dt) throws IllegalArgumentException {
 		if (!isValidDt(dt)) {
 			throw new IllegalArgumentException();
--- src/jumpingalien/model/Shark.java
+++ src/jumpingalien/model/Shark.java
@@ -2,7 +2,9 @@ package jumpingalien.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
+import be.kuleuven.cs.som.annotate.Basic;
 import jumpingalien.util.Sprite;
 
 public class Shark extends Enemy {
@@ -15,27 +17,108 @@ public class Shark extends Enemy {
 	}
 	
 	/**
-	 * Gets the time this shark is in the air.
+	 * Gets the number of periods this shark has not jumped.
 	 */
-	public double getTimeInAir(){
-		return this.time_in_air;
+	@Basic
+	public int getNonJumpingPeriods() {
+		return non_jumping_periods;
 	}
-	
+
 	/**
-	 * Sets the time this shark is in the air.
+	 * Sets the number of periods this shark has not jumped.
 	 * 
-	 * @param time
-	 * 			The new time this shark is in the air.
-	 * @post	The time this shark is in the air will be equal to time.
-	 * 			| new.getTimeInAir() = time 
+	 * @param nonjumpingperiods
+	 * 			the new number of periods this shark has not jumped.
+	 * @pre		...
+	 * 			| nonjumpinperiods >= 0
+	 * @post	...
+	 * 			| new.getNonJumpingPeriods() = nonjumpinperiods
 	 */
-	private void setTimeInAir(double time){
-		this.time_in_air = time;
+	public void setNonJumpingPeriods(int nonjumpingperiods) {
+		this.non_jumping_periods = non_jumping_periods;
+	}
+
+	private int non_jumping_periods;
+	
+	@Override
+	public void advanceTime(double time){
+		
+	}
+
+	@Override
+	protected void performRandomAction() {
+		performRandomHorizontalAction();
+		
+		
 	}
 	
-	private double time_in_air;
 	
+	protected void performRandomHorizontalAction(){
+		setVx(0);
+		Random rand = new Random();
+		if (rand.nextInt(2) == 0) 
+			setAx(getAxi());
+		else 
+			setAx(-getAxi());	
+	}
 	
+	protected void performRandomVerticalAction(){
+		setVy(0);
+		if(getNonJumpingPeriods() > 1)
+			setAy(0);
+		Random rand = new Random();
+		int nextaction;
+		if(getNonJumpingPeriods() > 4 && canJump())
+			nextaction = rand.nextInt(3);
+		else
+			nextaction = rand.nextInt(2);
+		
+		if(nextaction == 3){
+			startJump();
+		}						
+		else if( (nextaction  == 0) && canDiveOrRise()){
+			startRise();
+		}
+		else if(canDiveOrRise()){
+			startDive();
+		}		
+	}
+	
+	private boolean canDiveOrRise(){
+		ArrayList<List<List<Object>>> collisions = getWorld().collisionDetect(this, 0);
+		if(! collisions.get(3).get(1).contains(Feature.water) && ! submerged(collisions))
+			return true;
+		return false;
+	}
+	
+	private boolean submerged(ArrayList<List<List<Object>>> collisions){
+		if( collisions.get(1).get(1).contains(Feature.water))
+			return true;
+		return false;
+	}
+	
+	private void startJump(){
+		setVy(2);
+		setNonJumpingPeriods(0);
+	}
+	
+	private void startDive(){
+		setNonJumpingPeriods(getNonJumpingPeriods() + 1);
+		setAy(-0.2);
+	}
+	
+	private void startRise(){
+		setNonJumpingPeriods(getNonJumpingPeriods() + 1);
+		setAy(0.2);
+	}
+	
+
+	@Override
+	protected void terminate() {
+		assert(this.getWorld() != null);
+		this.getWorld().removeShark(this);
+		this.setWorld(null);		
+	}	
 	
 
 }
