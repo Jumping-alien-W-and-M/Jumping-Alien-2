c468b814009f855d5d26a697cb0f28e0e2f0b8d8
Michiel Bollen <michielbollen@student.kuleuven.be>
2015-04-06 12:55:33 +0200

    aanpassing aan shark

--- src/jumpingalien/model/Shark.java
+++ src/jumpingalien/model/Shark.java
@@ -48,7 +48,7 @@ public class Shark extends Enemy {
 	@Override
 	protected void performRandomAction() {
 		performRandomHorizontalAction();
-		
+		performRandomVerticalAction();
 		
 	}
 	
@@ -69,24 +69,20 @@ public class Shark extends Enemy {
 		Random rand = new Random();
 		int nextaction;
 		if(getNonJumpingPeriods() > 4 && canJump())
-			nextaction = rand.nextInt(3);
-		else
 			nextaction = rand.nextInt(2);
+		else
+			nextaction = rand.nextInt(1);
 		
-		if(nextaction == 3){
+		if(nextaction == 1){
 			startJump();
 		}						
-		else if( (nextaction  == 0) && canDiveOrRise()){
-			startRise();
-		}
-		else if(canDiveOrRise()){
-			startDive();
-		}		
+		else if(canDiveOrRise())
+			startRiseOrDive();	
 	}
 	
 	private boolean canDiveOrRise(){
 		ArrayList<List<List<Object>>> collisions = getWorld().collisionDetect(this, 0);
-		if(! collisions.get(3).get(1).contains(Feature.water) && ! submerged(collisions))
+		if(collisions.get(3).get(1).contains(Feature.water) || submerged(collisions))
 			return true;
 		return false;
 	}
@@ -97,19 +93,46 @@ public class Shark extends Enemy {
 		return false;
 	}
 	
+	private void startRiseOrDive(){
+		Random rand = new Random(); 
+		setNonJumpingPeriods(getNonJumpingPeriods() + 1);
+		double abs_value_ax = getAxi()*rand.nextDouble();
+		if(rand.nextInt(2) == 0)
+			setAx(-abs_value_ax);
+		else
+			setAx(abs_value_ax);
+	}
+	
+	@Override
+	public boolean canJump(){
+		ArrayList<List<List<Object>>> collisions = getWorld().collisionDetect(this, 0);
+		
+		if(collisions.get(3).get(1).contains(Feature.ground) || collisions.get(3).get(1).contains(Feature.water)
+				|| collisions.get(3).get(1).size() != 0)
+			return true;
+		
+		return false;
+	}
+	
+
+	@Override
+	protected double advanceAy() {
+		if(getAy() >= -0.2 && getAy() <= 0.2 && canDiveOrRise())
+			return(getAy());
+		if(getAy() == -10 && (! canJump() || ! submerged(getWorld().collisionDetect(this,0))))
+			return(getAy());
+		return 0;			
+	}
 	private void startJump(){
 		setVy(2);
 		setNonJumpingPeriods(0);
 	}
 	
-	private void startDive(){
-		setNonJumpingPeriods(getNonJumpingPeriods() + 1);
-		setAy(-0.2);
-	}
-	
-	private void startRise(){
-		setNonJumpingPeriods(getNonJumpingPeriods() + 1);
-		setAy(0.2);
+	@Override
+	protected double advanceVy(double time){
+		if(submerged(getWorld().collisionDetect(this,0)))
+			return 0;
+		return(super.advanceVy(time));
 	}
 	
 
