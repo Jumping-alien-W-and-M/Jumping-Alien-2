7005cca5feed4c0b7c205cbd139a65a7326d4d95
Wout <wout@wout>
2015-04-08 10:38:16 +0200

    Added FeatureTest.

---
+++ src/jumpingalien/model/FeatureTest.java
@@ -0,0 +1,35 @@
+package jumpingalien.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class FeatureTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void isPassableTest() {
+		assertEquals(Feature.air.isPassable(), true);
+		assertEquals(Feature.ground.isPassable(), false);
+		assertEquals(Feature.water.isPassable(), true);
+		assertEquals(Feature.magma.isPassable(), true);
+	}
+
+	@Test
+	public void numberTest() {
+		assertEquals(Feature.air.getNumber(), 0);
+		assertEquals(Feature.ground.getNumber(), 1);
+		assertEquals(Feature.water.getNumber(), 2);
+		assertEquals(Feature.magma.getNumber(), 3);
+	}
+
+}
