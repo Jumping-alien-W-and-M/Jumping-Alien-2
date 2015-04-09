1e61f97024f5cdd8d54b24d5b5de21537b1f57a3
Wout <wout@wout>
2015-04-09 23:22:15 +0200

    Plant tests - part 1.

--- src/jumpingalien/model/PlantTest.java
+++ src/jumpingalien/model/PlantTest.java
@@ -6,34 +6,46 @@ import jumpingalien.util.Sprite;
 import jumpingalien.util.Util;
 
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class PlantTest {
-	
-	private Plant plant;
-	Sprite[] sprites = {JumpingAlienSprites.ALIEN_SPRITESET[0], JumpingAlienSprites.ALIEN_SPRITESET[1]};
-
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
 
 	@Before
 	public void setUp() throws Exception {
-		plant = new Plant(0, 0, sprites);
+		test_world = new World(10, 20, 20, 200, 200, 19, 0);
+		test_plant = new Plant(0, 0, sprites);
+		test_world.addPlant(test_plant);
 	}
+	
+	private World test_world;
+	private Plant test_plant;
+	Sprite[] sprites = {JumpingAlienSprites.ALIEN_SPRITESET[0], JumpingAlienSprites.ALIEN_SPRITESET[1]};
 
 	@Test
 	public void TestConstructor() {
-		assertEquals(plant.getX(), 0, Util.DEFAULT_EPSILON);
-		assertEquals(plant.getY(), 0, Util.DEFAULT_EPSILON);
-		assertEquals(plant.getImages()[0], sprites[0]);
-		assertEquals(plant.getImages()[1], sprites[1]);
-		assertEquals(plant.getAxi(), 0, Util.DEFAULT_EPSILON);
-		assertEquals(plant.getVxi(), 0.5, Util.DEFAULT_EPSILON);
-		assertEquals(plant.getVxmax(), 0.5, Util.DEFAULT_EPSILON);
-		assertEquals(plant.getMovementTime(), 0, Util.DEFAULT_EPSILON);
-		assertEquals(plant.getVx(), plant.getVxi(), Util.DEFAULT_EPSILON);
+		assertEquals(test_plant.getX(), 0, Util.DEFAULT_EPSILON);
+		assertEquals(test_plant.getY(), 0, Util.DEFAULT_EPSILON);
+		assertEquals(test_plant.getImages()[0], sprites[0]);
+		assertEquals(test_plant.getImages()[1], sprites[1]);
+		assertEquals(test_plant.getAxi(), 0, Util.DEFAULT_EPSILON);
+		assertEquals(test_plant.getVxi(), 0.5, Util.DEFAULT_EPSILON);
+		assertEquals(test_plant.getVxmax(), 0.5, Util.DEFAULT_EPSILON);
+		assertEquals(test_plant.getMovementTime(), 0, Util.DEFAULT_EPSILON);
+		assertEquals(test_plant.getVx(), test_plant.getVxi(), Util.DEFAULT_EPSILON);
+	}
+
+	@Test
+	public void movementTimeTest() {
+		
+		for(int i = 0; i < 100; i++) {
+			assertEquals((i*0.1)%0.5, test_plant.getMovementTime(), Util.DEFAULT_EPSILON);
+			
+			if ((i%10) < 5) assertEquals(test_plant.getVxi(), test_plant.getVx(), Util.DEFAULT_EPSILON);
+			else assertEquals(-test_plant.getVxi(), test_plant.getVx(), Util.DEFAULT_EPSILON);
+			test_plant.advanceTime(0.1);
+			
+		}
+		
 	}
 
 }
