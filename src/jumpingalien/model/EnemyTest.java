00fc094691a90cd5824e9f9d2c2aae8bd4457cfb
Wout <wout@wout>
2015-04-09 20:49:44 +0200

    Completed EnemyTest.

--- src/jumpingalien/model/EnemyTest.java
+++ src/jumpingalien/model/EnemyTest.java
@@ -13,12 +13,14 @@ public class EnemyTest {
 
 	@Before
 	public void setUp() throws Exception {
+		test_world = new World(10, 200, 200, 200, 200, 19, 0);
+		
 		slime_sprites = new Sprite[2];
 		slime_sprites[0] = JumpingAlienSprites.ALIEN_SPRITESET[0];
 		slime_sprites[1] = JumpingAlienSprites.ALIEN_SPRITESET[1];
 		
 		test_school = new School();
-		test_school.setWorld(test_world);
+		test_world.addSchool(test_school);
 		
 		test_slime = new Slime(100, 200, slime_sprites, test_school);
 	}
@@ -34,26 +36,35 @@ public class EnemyTest {
 
 	@Test
 	public void constructorTest() {
-		assertEquals(test_slime.getMinActionTime(), 2, Util.DEFAULT_EPSILON);
-		assertEquals(test_slime.getMaxActionTime(), 6, Util.DEFAULT_EPSILON);
-		assertEquals(test_slime.getHitpoints(), 100);
+		assertEquals(2, test_slime.getMinActionTime(), Util.DEFAULT_EPSILON);
+		assertEquals(6, test_slime.getMaxActionTime(), Util.DEFAULT_EPSILON);
+		assertEquals(100, test_slime.getHitpoints());
 		
 		Shark shark = new Shark(100, 200, slime_sprites);
-		assertEquals(shark.getMinActionTime(), 1, Util.DEFAULT_EPSILON);
-		assertEquals(shark.getMaxActionTime(), 4, Util.DEFAULT_EPSILON);
-		assertEquals(shark.getHitpoints(), 100);
+		assertEquals(1, shark.getMinActionTime(), Util.DEFAULT_EPSILON);
+		assertEquals(4, shark.getMaxActionTime(), Util.DEFAULT_EPSILON);
+		assertEquals(100, shark.getHitpoints());
 	}
 
 	@Test
 	public void actionTimeTest() {
-		assertEquals(test_slime.getActionTime(), 0, Util.DEFAULT_EPSILON);
-		assertEquals(test_slime.getWorld(), test_world);
-		test_slime.advanceTime(0.1);
-		assertEquals(test_slime.getActionTime(), 0.1, Util.DEFAULT_EPSILON);
-		test_slime.advanceTime(0.1);
-		assertEquals(test_slime.getActionTime(), 0.2, Util.DEFAULT_EPSILON);
-		
-		// TO BE CONTINUED
+		for(int i = 0; i < 10; i++) {
+			test_slime = new Slime(1000, 0, slime_sprites, test_school);
+			
+			assertEquals(0, test_slime.getActionTime(), Util.DEFAULT_EPSILON);
+			test_slime.advanceTime(0.1);
+			double action_time = test_slime.getActionTime();
+			assert(action_time >= test_slime.getMinActionTime() - 0.1);
+			assert(action_time < test_slime.getMaxActionTime() - 0.1);
+			
+			for(double j = 0; (j < 3) && (action_time > 0.15); j++) {
+				action_time -= 0.15;
+				test_slime.advanceTime(0.15);
+				assertEquals(action_time, test_slime.getActionTime(), Util.DEFAULT_EPSILON);
+			}
+			
+			test_school.removeSlime(test_slime);
+		}
 	}
 
 }
