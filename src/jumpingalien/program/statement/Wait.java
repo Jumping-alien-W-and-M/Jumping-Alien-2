debd9b921b46947c1cc6159f641506f8b415e7bb
Wout <wout@wout>
2015-05-11 15:21:46 +0200

    Added execute in wait.

--- src/jumpingalien/program/statement/Wait.java
+++ src/jumpingalien/program/statement/Wait.java
@@ -1,6 +1,10 @@
 package jumpingalien.program.statement;
 
+import be.kuleuven.cs.som.annotate.Basic;
+import jumpingalien.model.Program;
 import jumpingalien.part3.programs.SourceLocation;
+import jumpingalien.program.ProgramExecutor;
+import jumpingalien.program.Type;
 import jumpingalien.program.expression.*;
 
 public class Wait extends Statement {
@@ -9,26 +13,41 @@ public class Wait extends Statement {
 		super(sourceLocation);
 		
 		this.duration = duration;
+		
+		if(duration.getType() != Type.DOUBLE) Program.printTypeCheckError(sourceLocation);
 	}
 	
+	@Basic
 	public Expression getDuration() {
 		return this.duration;
 	}
 	
 	private final Expression duration;
 	
+	@Basic
 	public double getDurationLeft() {
 		return this.duration_left;
 	}
 	
-	private double duration_left;
+	private void setDurationLeft(double duration_left) {
+		this.duration_left = duration_left;
+	}
+	
+	private double duration_left = 0;
 	
 	@Override
 	public boolean execute() {
 		
+		if (getDurationLeft() <= 0) {
+			setDurationLeft((double) getDuration().getValue());
+		}
 		
+		while(ProgramExecutor.getStatementsLeft() > 0 && getDurationLeft() > 0) {
+			ProgramExecutor.setStatementsLeft(ProgramExecutor.getStatementsLeft() - 1);
+			setDurationLeft(getDurationLeft() - 0.001);
+		}
 		
-		return true;
+		return (getDurationLeft() <= 0);
 	}
 	
 }
