1823d4a914f2169831e07ffc4eabe5c910e2860d
Wout <wout@wout>
2015-05-20 18:01:55 +0200

    Added uncompleted SqrtTest.

---
+++ src/jumpingalien/program/expression/unary/SqrtTest.java
@@ -0,0 +1,19 @@
+package jumpingalien.program.expression.unary;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SqrtTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
