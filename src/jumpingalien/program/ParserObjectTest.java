4834b8d51b2eaf8a4c10f2d9a065c2b67df98aac
Wout <wout@wout>
2015-05-20 14:51:13 +0200

    Completed tests for jumpingalien.program.

---
+++ src/jumpingalien/program/ParserObjectTest.java
@@ -0,0 +1,27 @@
+package jumpingalien.program;
+
+import static org.junit.Assert.*;
+import jumpingalien.part3.programs.SourceLocation;
+import jumpingalien.program.expression.Null;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ParserObjectTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void BasicConstructorTest() {
+		ParserObject Null;
+		SourceLocation source = new SourceLocation(5, 6);
+		
+		Null = new Null(null);
+		assertEquals(Null.getSourceLocation(), null);
+		Null = new Null(source);
+		assertEquals(Null.getSourceLocation(), source);
+	}
+
+}
