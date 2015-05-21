7dd4209710d63ca4ed578d622f79fc590584aa4f
Wout <wout@wout>
2015-05-21 17:03:59 +0200

    Added WaitTest.

--- src/jumpingalien/program/statement/Wait.java
+++ src/jumpingalien/program/statement/Wait.java
@@ -38,16 +38,19 @@ public class Wait extends ActionStatement {
 	@Override
 	public ExecutionState execute(GameObject executingObject) {
 		
-		if (getDurationLeft() <= 0) {
+		if (getDurationLeft() <= 0.000001) {
 			setDurationLeft((double) getDuration().getValue(executingObject));
 		}
+
+		executingObject.getProgram().setStatementsLeft(executingObject.getProgram().getStatementsLeft() - 1);
+		setDurationLeft(getDurationLeft() - 0.001);
 		
 		while(executingObject.getProgram().getStatementsLeft() > 0 && getDurationLeft() > 0) {
 			executingObject.getProgram().setStatementsLeft(executingObject.getProgram().getStatementsLeft() - 1);
 			setDurationLeft(getDurationLeft() - 0.001);
 		}
 		
-		if (getDurationLeft() <= 0) return ExecutionState.DONE;
+		if (getDurationLeft() <= 0.000001) return ExecutionState.DONE;
 		else return ExecutionState.NOTDONE;
 	}
 	
